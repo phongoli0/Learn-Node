1b0b6e99b8571c69d4cf77810a31fa0f8a046e53
Phong Truong <phongoli0@gmail.com>
2019-10-03 16:39:07 -0700

    green

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,22 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // console.log("yo!");
+  // res.send('Hey! It works! i guess');
+  const phong = {name: "phong", age: 30};
+  // res.json(phong);
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render('hello', {
+    name: 'phong',
+    dog: "bella",
+    title: "i love food",
+  });
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+})
 
 module.exports = router;
